b21b25f6bcd492f110c8d5d983716d7a67298f77
dossy007 <toshi@toshinoMacBook-ea.local>
2018-10-22 13:10:44 +0900

    Add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ##Assosiation
+  has_many :members
+  has_many :messages
+  has_many :groups, through: :members
 end
