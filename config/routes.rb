24b0e0121abca0c99f9c954d9a748c63bdbf6ebd
dossy007 <toshi@toshinoMacBook-ea.local>
2018-10-25 19:10:27 +0900

    Add resources groups & messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index"
   resources :users
+  resources :groups
+  	resources :messages
+  	#groups_idをmessageに渡す。
+  end
 end
