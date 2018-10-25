755211b63ea29beac20466ef2c9318a414775500
dossy007 <toshi@toshinoMacBook-ea.local>
2018-10-25 19:02:35 +0900

    Edit foreign_key

--- db/migrate/20181025094658_create_members.rb
+++ db/migrate/20181025094658_create_members.rb
@@ -1,8 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.1]
   def change
     create_table :members do |t|
-      t.references :group, foreign_key :true
-      t.references :user, foreign_key :true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
