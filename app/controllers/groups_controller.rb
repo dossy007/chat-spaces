0704d9c0b4f326d5460853cbafe565f5cf6e04e4
dossy007 <toshi@toshinoMacBook-ea.local>
2018-10-28 14:15:11 +0900

    Add action new & create
    create_paramsも追加。
    createするときに、strong_parameterを渡した。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,11 +3,25 @@ class GroupsController < ApplicationController
   end
 
   def new
+  	@group = Group.new
+  	@group.users << current_user
   end
 
   def create
+  	@group = Group.new(create_params)
+  	if @group.save
+  		redirect_to root_path, notice: "グループを作成しました"
+  	else
+  		render :new
+  	end
   end
 
   def delete
   end
+
+  private
+  def create_params
+  	params.require(:group).permit(:name,{:user_ids => []})
+  end
+
 end
