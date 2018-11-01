77ea7cdb69eaaf703cb436c47ad1a55b04040de6
dossy007 <toshi@toshinoMacBook-ea.local>
2018-11-01 19:54:50 +0900

    Edit create & before_action
    Createアクションを設定。
    flashも設定。
    ストロングパラも設定し、before_actionでidを渡せるように

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,34 @@
 class MessagesController < ApplicationController
+	before_action :set_group
+	#beforeにgroupをクリックした時、そのgroup_idを渡してあげる。
 	def index
+		@message = Message.new
+		@messages = @group.messages.includes(:user)
 	end
 
 	def new
 	end
 
 	def create
+		@message = @group.messages.new(message_params)
+		if @message.save
+			redirect_to group_messages_path(@group), notice: 'messageが送信'
+			# redirect_toの第二引数にnoticeのキーを取れる flash[:notice]と同義
+		else
+			@messages = @group.messages.includes(:user)
+			flash.now[:alert] = 'meaagesを入力してください'
+			render :index
+		end
 	end
 
 	def delete
+	end
+
+	private
+	def message_params
+		params.require(:message).permit(:content,:image).merge(user_id: current_user.id)
+	end
+	def set_group
+		@group = Group.find(params[:group_id])
 	end
 end
