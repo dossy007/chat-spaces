de6df54d2a2c10d3c21f49fd5c656b5c0e5096ac
dossy007 <toshi@toshinoMacBook-ea.local>
2018-11-04 10:39:19 +0900

    edit user
    userにonlyを付け加えた。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
-  resources :users
+  resources :user, only: [:edit, :update]
   resources :groups do
   	resources :messages
   	#groups_idをmessageに渡す。
