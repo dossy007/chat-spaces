8f3ee8031f75a639ac919c68a847675378b72c71
dossy007 <toshi@toshinoMacBook-ea.local>
2018-12-13 20:59:17 +0900

    Edit rspec
    Rails sできなかったので、rspecあたりをいじった。
    どうやら、gitignoreにimageのところをかくと（無視すると）rspecが動かなくなり、serverも動かなくなるという現象が怒った。
    これで、rails sができるようになった

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :message do
     text Faker::Lorem.sentence
     # Fakerでダミーを作成している。
-    image File.open("#{Rails.root}/public/uploads/message/image/27/あか時計.jpg")
+    image File.open("#{Rails.root}/public/uploads/message/image/38/あか時計.jpg")
     # image File.open("#{Rails.root}/public/images/no_image.jpg")
 
     #これは、何をやっているのか？
