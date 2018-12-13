94f020106d9e924913607e6c6affb1aca0781cd5
dossy007 <toshi@toshinoMacBook-ea.local>
2018-12-14 00:12:41 +0900

    First commit

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,13 +1,33 @@
 // formが送信された時にイベントが動くようにしよう。
 
 $(function() {
-	function() {
-		// htmlとか
+	function buildHTML(message) {
+		var html = `render 'chat-main__body--message'`
+		return html;
 	}
-
-	$('#submit').on('submit',function(e) {
+	$('#new_comment').on('submit',function(e) {
 		e.preventDefault();  //リンク推移を無効に
 		var formData = new FormData(this);  //thisはid=submit
-		console.log(this);
-	})
+		var url = $(this).attr('action');
+
+		$ajax({
+			url: url,
+			type: "POST",
+			data: formData,
+			dataType: 'json',
+			processData: false,
+			contentType: false,
+		})
+		.done(function(data) {
+			var html = buildHTML(data);
+		})
+		.fail(function() {
+			alert('error');
+			console.log(this)
+		    console.log(html);
+
+		})
+	});
 });
+
+
