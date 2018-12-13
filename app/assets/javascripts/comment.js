37c21bc6845ce364d6210da5c1a68fa134318dcc
dossy007 <toshi@toshinoMacBook-ea.local>
2018-12-13 20:57:06 +0900

    crate comment.js
    非同期処理を書く。ところを追加。
    作成途中。

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,1 +1,13 @@
-js
+// formが送信された時にイベントが動くようにしよう。
+
+$(function() {
+	function() {
+		// htmlとか
+	}
+
+	$('#submit').on('submit',function(e) {
+		e.preventDefault();  //リンク推移を無効に
+		var formData = new FormData(this);  //thisはid=submit
+		console.log(this);
+	})
+});
