c04082a51d2dab8d8f9dfcffe0f3473116088b2a
dossy007 <toshi@toshinoMacBook-ea.local>
2018-12-15 20:31:09 +0900

    add ajax
    Ajax通信に関する処理を書いた。
    Submitが送られたら、発火する処理。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,61 @@
+// formが送信された時にイベントが動くようにしよう。
+
+// $(function() {
+// 	function buildHTML(message) {
+// 		var html = `<%= render :partial => "chat-main__body--message" %>`
+// 	// 	var html = `%div.chat-main__body--message
+// 	// 			   - messages.each do |message|
+// 	// 	            %ul
+// 	// 	              %li.chat-main__body--message-name
+// 	// 		            = message.user.name
+// 	// 		          %li.chat-main__body--message-date
+// 	// 		            = message.created_at.strftime('%Y年%m月%d日 %H:%M:%S')
+// 	// 		          %li.chat-main__body--message-text
+// 	// 		            - if message.text.present?
+// 	// 		              = message.text
+// 	// 		            = image_tag message.image.url, class: 'lower' if message.image.present?`
+
+// 		return html;
+// 	}
+// 	$('#new_comment').on('submit',function(e) {
+// 		e.preventDefault();  //リンク推移を無効に
+// 		var formData = new FormData(this);  //thisはid=submit
+// 		var url = $(this).attr('action');
+// 		$.ajax({
+// 			url: url,
+// 			type: "POST",
+// 			data: formData,
+// 			dataType: 'json',
+// 			contentType: false
+// 		})
+// 	});
+// });
+
+
+$(function() {
+	function buildHTML(message) {
+		var html = `<%= render :partilal => "chat-main__body--message"%>`
+		return html
+	}
+	$('#new_message').on('submit',function(e) {
+		e.preventDefault();
+		var formData = new FormData(this);
+		var url = $(this).attr('action');
+		$.ajax( {
+			url: url,
+			type: 'POST',//"POST"?
+		    data: formData,
+		    datatype: 'json',
+		    processData: false,
+		    contentType: false
+		})
+		.done(function(data) {
+			console.log(url);
+			var html = buildHTML(data);
+			$('.chat-main__body--message').append(html);
+		})
+		.fail(function() {
+			alert('error')
+		})
+	})
+});
