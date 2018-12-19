09d69cadb8cca5ca501406af4e55301340588e11
dossy007 <toshi@toshinoMacBook-ea.local>
2018-12-19 14:32:28 +0900

    Add html
    var htmlを追加。
    非同期で追加させるため。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,9 +34,45 @@
 
 $(function() {
 	function buildHTML(message) {
-		var html = `<%= render :partilal => "chat-main__body--message"%>`
-		return html
-	}
+	// 	var html = `<%= render :partilal => "chat-main__body--message"%>`
+	// 	var html =`.chat-main__body--message
+ //  - messages.each do |message|
+	// %ul
+	//   %li.chat-main__body--message-name
+	//     = message.user.name
+	//   %li.chat-main__body--message-date
+	//     = message.created_at.strftime('%Y年%m月%d日 %H:%M:%S')
+	//   %li.chat-main__body--message-text
+	//     - if message.text.present?
+	//       = message.text
+	//     = image_tag message.image.url, class: 'lower' if message.image.present?`
+
+	// var user_name = message.user.name
+	// userの情報が引っ張ってこれない。
+	var html = `
+    <ul>
+      <li class="chat-main__body--message-name">
+        <%= message.user.name %>
+        ${message.user_name}
+      </li>
+      <li class="chat-main__body--message-date">
+        ${message.created_at}
+      </li>
+      <li class="chat-main__body--message-text">
+	      if message.text.present?
+	        ${message.text}
+	      else
+	        <%= image.tag message.image.url, class="lower" if message.image.present? %>
+	    ${message.user_id}
+	    ${message.id}
+	      end
+	    }
+	    }
+	    }
+      </li>
+    </ul>`
+		return html;
+	};
 	$('#new_message').on('submit',function(e) {
 		e.preventDefault();
 		var formData = new FormData(this);
@@ -50,11 +86,11 @@ $(function() {
 		    contentType: false
 		})
 		.done(function(data) {
-			console.log(url);
-			var html = buildHTML(data);
-			// $('.chat-main__body--message').append(html);
 			console.log(html);
-			$('.chat-main__body').animate({scrollTop: 0}, 500,'swing' )
+			var html = buildHTML(data);
+			$('.chat-main__body--message').append(html);
+			console.log(html);
+			
 		})
 
 		.fail(function() {
