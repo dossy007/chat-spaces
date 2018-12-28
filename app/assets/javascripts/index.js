19606781cb86961cba7a4ded0dd2913b56b31d11
dossy007 <toshi@toshinoMacBook-ea.local>
2018-12-28 21:55:31 +0900

    add .ajax
    Ajax通信を行うための処理を追記

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -15,6 +15,13 @@ $(function() {
 	}
 	$("#user-search-field").on("keyup", function() {
 		var input = $("#user-search-field").val();
-		console.log(input)
+		console.log(input);
+
+		$.ajax( {
+			type: 'GET',
+			url: '/users',
+			data: {keyword: input},
+			dataType: 'json'
+		})
 	})
 })
