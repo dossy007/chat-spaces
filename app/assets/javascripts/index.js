0db645177472b13e718f9fea683b62ff775f79e0
dossy007 <toshi@toshinoMacBook-ea.local>
2018-12-28 20:50:47 +0900

    add append html
    インクリ時にhtmlを追加する処理

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,14 @@
 $(function) { //大枠
 
+	function appendUsers(user) { //htmlを加える処理
+		var html = `<div class='chat-group-form__field--right'>
+					  <div class='chat-group-form__search clearfix'>
+					    <input class='chat-group-form__input' id='user-search-field' placeholder='追加したいユーザー名を入力してください' type='text'>
+					  </div>
+					  <div id='user-search-result'></div>
+					</div>`
+	};
+
+	
+	$(".chat-group-form__input").on("keyup")
 }
