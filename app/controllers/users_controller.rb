438de5dfe754a896c0f2b3556963fac8d9ce6605
dossy007 <toshi@toshinoMacBook-ea.local>
2019-01-25 10:01:49 +0900

    search current_user.id
    current_user.idを外して、検索。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)',"%#{params[:name]}%").where.not(id: current_user.id)
+    @users = User.where('name LIKE(?) and id != ?',"%#{params[:name]}%",current_user.id)
     respond_to do |format|
       format.html
       format.json
