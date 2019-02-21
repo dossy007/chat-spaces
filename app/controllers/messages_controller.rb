59331b708a87e5c4f36da7596c9024ab3e122a39
dossy007 <toshi@toshinoMacBook-ea.local>
2019-02-21 14:02:28 +0900

    edit @new_messages respond

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
 		@messages = @group.messages.includes(:user)
 		respond_to do |format|
 			format.html
-			format.json{@new_messages = Message.where('id > ?', params[:message][:id])}
+			format.json{ @new_messages = Message.where('id > ?', params[:message][:id]) }
 		end
 	end
 
@@ -15,15 +15,15 @@ class MessagesController < ApplicationController
 
 	def create
 		@message = @group.messages.new(message_params)
-		respond_to do |format|
-			if @message.save
+		if @message.save
+			respond_to do |format|
 				format.json
-			else
-				@messages = @group.messages.includes(:user)
-				flash.now[:alert] = 'messagesを入力してください'
-				render :index
-			end
-	    end
+		    end
+		else
+			@messages = @group.messages.includes(:user)
+			flash.now[:alert] = 'messagesを入力してください'
+			render :index
+		end
 	end
 
 	def delete
