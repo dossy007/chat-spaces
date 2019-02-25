21ff023e2622e39bdda9e6e8e8e8df47b3bf35bb
dossy007 <toshi@toshinoMacBook-ea.local>
2019-02-25 15:02:45 +0900

    Edit set rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
-
+set :rbenv_path, '/home/deploy.rbenv'
+set :bundle_path, './vendor/bundle'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)}#{fetch(:rbenv_path)}/bin/rbenv exec"
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/dossy.pem']
 
