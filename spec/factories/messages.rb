226c696c5a6ea11cafae00f4caf592faa555a0e7
dossy007 <toshi@toshinoMacBook-ea.local>
2019-02-26 20:34:42 +0900

    Edit rails 5 fakder
    Rails 5ようにfakerの記述を変更

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :message do
-    text Faker::Lorem.sentence
+    text {Faker::Lorem.sentence}
     # Fakerでダミーを作成している。
-    image File.open("#{Rails.root}/public/uploads/message/image/38/あか時計.jpg")
+    content {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/uploads/message/image/38/あか時計.jpg")}
     # image File.open("#{Rails.root}/public/images/no_image.jpg")
 
-    #これは、何をやっているのか？
+    #これは、何をやっているのか？f
   end
 end
