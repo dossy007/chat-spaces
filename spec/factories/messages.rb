4b1b6685d2c957954ccf9fb3cd4f6936fcccb450
dossy007 <toshi@toshinoMacBook-ea.local>
2019-02-28 13:17:33 +0900

    Change images folder

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     text {Faker::Lorem.sentence}
     # Fakerでダミーを作成している。
     content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/uploads/message/image/38/あか時計.jpg")}
+    image {File.open("#{Rails.root}/public/images/1.png")}
     # image File.open("#{Rails.root}/public/images/no_image.jpg")
 
     #これは、何をやっているのか？f
